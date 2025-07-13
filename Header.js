03b63e8f75952ca454dd4a1e4316046d846a9a79
Robin Singh <trsrobin1@gmail.com>
2025-07-13 21:09:31 +0530

    Update and rename components to Header.js

---
+++ Header.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const Header = () => {
+    const cart = useSelector(state => state.cart);
+    const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+
+    return (
+        <header>
+            <h1>Houseplant Haven</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/cart">Cart ({totalItems})</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
